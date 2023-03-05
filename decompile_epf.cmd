b23cc1ece3f4d49a61c8cc3d07a5ac8252be414a
VladimirUkolov <igrovert@icloud.com>
2023-03-05 18:39:39 +0300

    доработаны прил 1 и прил2 этап 2

--- decompile_epf.cmd
+++ decompile_epf.cmd
@@ -1,5 +1,5 @@
 @chcp 65001
-call vrunner session lock --settings devN.json
-call vrunner session kill --settings devN.json
+@rem call vrunner session lock --settings devN.json
+@rem call vrunner session kill --settings devN.json
 call vrunner decompileepf .\build\epf .\src\epf --settings devN.json
-call vrunner session unlock --settings devN.json
+@rem call vrunner session unlock --settings devN.json
